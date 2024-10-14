feeb4134ed3ec8aa4ee764f03102c61656272707
AltairGeo <altairgeo000@gmail.com>
2024-10-14 19:00:08 +0300

    Logging added

--- src/bot.py
+++ src/bot.py
@@ -12,6 +12,7 @@ import feedparser
 import pytz
 from io import BytesIO
 from typing import Optional
+from log import logging
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -53,11 +54,13 @@ bot = discord.Bot(intents=intents)
 @bot.event
 async def on_ready():
     print("Bot started!")
+    logging.info("Bot started")
     post_q = collections.deque(maxlen=120)
     httpx_client = httpx.AsyncClient()
     num_of_test_char = 70
     # вызов асинхронного парсера rss ленты и выставление параметров
     await meduza_news(num_of_test_char, post_q, httpx_client)
+    logging.info("Meduza parser is started")
 
 
 @bot.event
@@ -76,7 +79,9 @@ async def meduza_news(num_of_test_char, post_query, httpx_client):
     while True:
         try:
             resurs = await httpx_client.get(rss_link)
+            logging.debug("Meduza parser, request fulfilled ")
         except:
+            logging.warning("Falls request. Meduza parser")
             await asyncio.sleep(10)
             continue
 
@@ -96,6 +101,7 @@ async def meduza_news(num_of_test_char, post_query, httpx_client):
             post_query.appendleft(head)
             #print(news_text)
             if firstly_indicator == 1:
+                logging.debug("One of the newsletters has been sent")
                 await channel.send(news_text)
         firstly_indicator = 1
         await asyncio.sleep(20)
@@ -119,12 +125,14 @@ async def gtn(ctx):
 async def dollarcost(ctx):
     cost = await func.get_dollar_cost(None)
     stor = f"1$ = {cost}₽"
+    logging.info("The /dollarcost was used")
     await ctx.respond(stor)
 
 
 # Дать предупреждение
 @bot.slash_command(description="Дать предупреждение пользователю.")
 async def addwarn(ctx, name: discord.Member, reason: str):
+    logging.info("the /addwarn was used")
     author_roles = ctx.author.roles
     mod = config.MOD_ID
     right = 0
@@ -134,6 +142,7 @@ async def addwarn(ctx, name: discord.Member, reason: str):
             user_id = name.id
             user_name = name.name
             await ctx.respond(warn.add_warn(user_id, user_name, reason))
+            logging.debug(f"Add warn to {user_name} for reason: {reason}")
             right = 1
     if right == 0:
         await ctx.respond("У вас нет прав на выполнение данной команды!")
@@ -152,6 +161,7 @@ async def addwarn(ctx, name: discord.Member, reason: str):
 
 @bot.slash_command(description="Удалить предупреждение у пользователя по id.")
 async def delwarn(ctx, id_warn: int):
+    logging.info("the /delwarn was used")
     author_roles = ctx.author.roles
     mod = config.MOD_ID
     right = 0
@@ -159,6 +169,7 @@ async def delwarn(ctx, id_warn: int):
         i = i.id
         if str(i) == mod:
             await ctx.respond(warn.delete_warn(id_warn))
+            logging.debug(f"Deleted warning with id = {id_warn}")
             right = 1
     if right == 0:
         await ctx.respond("У вас нет прав на выполнение данной команды!")
@@ -167,12 +178,14 @@ async def delwarn(ctx, id_warn: int):
 # Все предупреждения пользователя
 @bot.slash_command(description="Показать все предупреждения пользователя")
 async def alluserwarn(ctx, name: discord.Member):
+    logging.info("the /alluserwarn was used")
     await ctx.respond(warn.all_user_warn(name.id))
 
 
 # мут
 @bot.slash_command(description="Дать мут на определённое кол-во часов.")
 async def mute(ctx, name: discord.Member, hours: int):
+    logging.info("the /mute was used")
     delta = timedelta(hours=hours)
     author_roles = ctx.author.roles
     mod = config.MOD_ID
@@ -181,6 +194,7 @@ async def mute(ctx, name: discord.Member, hours: int):
         i = i.id
         if str(i) == mod:
             await name.timeout_for(delta)
+            logging.debug(f"{name.name} is get mute for {hours} hours.")
             await ctx.respond(f"Дан мут пользователю {name.name}!")
             right = 1
     if right == 0:
@@ -190,6 +204,7 @@ async def mute(ctx, name: discord.Member, hours: int):
 #Модуль рандомной статьи из вики 
 @bot.slash_command(description="Рандомная статья из википедии")
 async def grws(ctx):
+    logging.info("the /grws was used")
     if config.ENABLE_WIKI_MODULE == 1:
         wiki_engine = wl.WikiLib(config.WIKI_URL)
         name = wiki_engine.get_name()
@@ -205,26 +220,30 @@ async def grws(ctx):
                 break
             result += i
         result += f"\n {link}"
-        
+        logging.debug("grws engine complete parsing")
         embed = discord.Embed(title='Случайная статья из википедии', color=discord.Color.green())
         embed.add_field(name='Содержимое', value=result, inline=False)
         try:       
             embed.set_image(url=wiki_engine.get_picture())
         except Exception as e:
             print(e)
+        logging.debug("grws created a embed")
         await ctx.respond(embed=embed)
     else:
+        logging.warning("ENABLE_WIKI_MODULE = 0")
         await ctx.respond("Вики-Модуль выключен!")
 
 
 @bot.slash_command(description="Переводчик")
 async def translate(ctx, message: str):
+    logging.info("the /translate was used")
     await ctx.respond(view=TranslatorView(messages=message))
 
 
 # Запрос к нейросети нужен ollama сервер, и указать модель в config.py
 @bot.slash_command(description="ИИ")
 async def ai(ctx, prompt: str):
+    logging.info("the /ai was used")
     if config.ENABLE_OLLAMA_MODULE == 1:
         await ctx.respond("Обработка...")
         async with ctx.channel.typing():
@@ -236,6 +255,7 @@ async def ai(ctx, prompt: str):
 
 @bot.slash_command()
 async def ai_forget_context(ctx):
+    logging.info("the /ai_forget_context was used")
     if config.ENABLE_OLLAMA_MODULE == 1:
         await ctx.send(func.ai_forget())
     else:
@@ -248,16 +268,19 @@ async def ai_forget_context(ctx):
 
 @bot.slash_command()
 async def flip(ctx):
+    logging.info("the /flip was used")
     await ctx.respond(await func.flip())
 
 
 @bot.slash_command(description="Количество участников на сервере.")
 async def members_count(ctx):
+    logging.info("the /membrers_count was used")
     await ctx.respond(f"На сервере {ctx.guild.member_count}")
 
 
 @bot.slash_command()
 async def fox(ctx):
+    logging.info("the /fox was used")
     ap = func.API_r()
     resp = await ap.get_request_json(atr="image", url="https://randomfox.ca/floof/")
     await ctx.respond(resp)
@@ -265,6 +288,7 @@ async def fox(ctx):
 
 @bot.slash_command()
 async def yes_gif(ctx):
+    logging.info("the /yes_gif was used")
     ap = func.API_r()
     resp = await ap.get_request_json(atr="image", url="https://yesno.wtf/api?force=yes")
     await ctx.respond(resp)
@@ -272,6 +296,7 @@ async def yes_gif(ctx):
 
 @bot.slash_command()
 async def no_gif(ctx):
+    logging.info("the /no_gif was used")
     ap = func.API_r()
     resp = await ap.get_request_json(atr="image", url="https://yesno.wtf/api?force=no")
     await ctx.respond(resp)
@@ -279,52 +304,65 @@ async def no_gif(ctx):
 
 @bot.slash_command()
 async def weather(ctx, city: str):
+    logging.info("the /weather was used")
     ct = city
     city = city.replace(" ", "+")
     url = f"https://wttr.in/{city}?Q0n&lang=ru"
+    logging.debug("Weather: url formed")
     api = func.API_r()
     try:
         await ctx.respond("Обработка...")
+        logging.debug("Weather: send request for wttr.in")
         resp = await api.get_request(url)
-        embed = discord.Embed(title=f'Погода в {ct}', description=f"```{resp.text}```", color=discord.Color.green())
+        embed = discord.Embed(title=f'Погода в {ct}', description=f"```ansi\n{resp.text}```", color=discord.Color.green())
+        logging.debug("Weather: embed formed")
         await ctx.send(embed=embed)
     except Exception as e:
+        logging.error(f"Weather error: {e}")
         await ctx.send(f"Ошибка: {e}")
 
 
 @bot.slash_command()
 async def moon(ctx):
+    logging.info("the /moon was used")
     url = "https://wttr.in/moon?T0&lang=ru"
     api = func.API_r()
     try:
         await ctx.respond("Обработка...")
+        logging.debug("moon: Send request to wttr.in")
         resp = await api.get_request(url)
         print(resp.text)
         #embed = discord.Embed(title=f'Фаза луны', description=f"```ansi\n{resp.text}```", color=discord.Color.green())
         #await ctx.send(embed=embed)
         await ctx.send(f"```ansi\n{resp.text}```")
     except Exception as e:
+        logging.error(f"moon error: {e}")
         await ctx.send(f"Ошибка: {e}")
 
 
 #http://numbersapi.com/
 @bot.slash_command()
 async def fact_about_number(ctx, num: int):
+    logging.info("the /fact_about_number was used")
     url = f"http://numbersapi.com/{str(num)}"
     try:
         api = func.API_r()
+        logging.debug("Send request to numbersapi.com")
         resp = await api.get_request(url)
         translatorr = Translator(from_lang='en', to_lang='ru')
         respond = f"EN: {resp.text}\nRU: {translatorr.translate(resp.text)}"
         await ctx.respond(respond)
     except Exception as e:
+        logging.error(f"fact_about_number error: {e}")
         await ctx.send(f"Ошибка! {e}")
 
 
 #https://catfact.ninja/fact
 @bot.slash_command()
 async def cat_fact(ctx):
+    logging.info("the /cat_fact was used")
     api = func.API_r()
+    logging.debug("cat_fact: send request to catfact.ninja/fact")
     resp = await api.get_request_json(atr="fact", url="https://catfact.ninja/fact")
     translatorr = Translator(from_lang='en', to_lang='ru')
     await ctx.respond(f"EN: {resp}\n-------------------------------------------------------------------\nRU: {translatorr.translate(resp)}")
@@ -339,10 +377,14 @@ async def fetch_image(url):
 # ISS location
 @bot.slash_command()
 async def iss_location(ctx):
+    logging.info("the /iss_location was used")
     await ctx.respond("Обработка...")
     try:
+        logging.debug("iss_location: try to request iss location")
         iss = await func.get_iss_loc()
+        logging.debug("iss_location: try to request url for map image")
         api_url = await func.link_iss_map_form(iss['latitude'], iss['longitude'])
+        logging.debug("iss_location: try to fetcg map image")
         image_data = await fetch_image(api_url)
         files = discord.File(BytesIO(image_data), filename='image.png')
         await ctx.send(file=files)
@@ -353,24 +395,28 @@ async def iss_location(ctx):
 # Количество людей в космосе
 @bot.slash_command()
 async def people_in_space(ctx):
+    logging.info("the /people_in_space was used")
     await ctx.respond("Обработка...")
     url = "http://api.open-notify.org/astros.json"
     try:
         empty = " "
         api = func.API_r()
+        logging.debug("people_in_space: try to get people list")
         resp = await api.get_request_json_raw(url)
         people = "```\n+---------------------------+---------------------------------------+\n|           Имя             |               Станция                 |\n+---------------------------+---------------------------------------+\n"
         for i in resp['people']:
             people += f"|{empty * 3}{i['name']}{empty * (24 - len(i['name']))}|{empty * 3}{i['craft']}{empty * (36 - len(i['craft']))}|\n"
         final = f"# На данный момент в космосе {str(resp['number'])} человек вот их список:satellite_orbital::\n{people}+---------------------------+---------------------------------------+```"
-    
+        logging.debug("people_in_space: complete list forming")
         await ctx.send(final)
     except Exception as e:
+        logging.error(f"people_in_space: ERROR! {e}")
         await ctx.send(f"Ошибка! Подробнее: {e}")
 
 
 @bot.slash_command()
 async def i_moder(ctx):
+    logging.info("the /i_moder was used")
     resp = await func.moder(ctx)
     await ctx.respond(str(resp))
 
@@ -378,9 +424,11 @@ async def i_moder(ctx):
 # Статистика сообщений за месяц
 @bot.slash_command()
 async def month_statistic(ctx, year: int, month: int):
+    logging.info("the /month_statistic was used")
     moder = await func.moder(ctx)
     if moder == True:
         await ctx.respond("Обработка...")
+        logging.debug("month_statistic: get hist for month")
         resp = await func.get_count_hist_for_mouth(month, year)
         if resp == None:
             await ctx.respond("Ничего не найдено.")
@@ -392,9 +440,11 @@ async def month_statistic(ctx, year: int, month: int):
 
 @bot.slash_command()
 async def channel_statistics(ctx, year: int, month: int):
+    logging.info("the /channel_statistics was used")
     moder = await func.moder(ctx)
     if moder == True:
         await ctx.respond("Обработка...")
+        logging.debug("channel_statistic: get channel statistic")
         resp = await func.get_channels_statistic(month, year)
         if resp == None:
             await ctx.respond("Ничего не найдено.")
@@ -411,9 +461,11 @@ async def channel_statistics(ctx, year: int, month: int):
 
 @bot.slash_command()
 async def get_day_hist(ctx, day: int, mounth: int, year: int):
+    logging.info("the /get_day_hist was used")
     await ctx.respond("Обработка...")
     moder = await func.moder(ctx)
     if moder == True:
+        logging.debug("get_day_hist: get all history for day")
         ls = await func.get_hist_for_day(day, mounth, year)
         if ls == []:
             await ctx.send("Ничего не найдено.")
@@ -430,6 +482,7 @@ async def get_day_hist(ctx, day: int, mounth: int, year: int):
 
 @bot.listen()
 async def on_message(message: discord.Message):
+    logging.info("Message processing")
     author, author_id, content, chanel, chanel_id = message.author.name, message.author.id, message.content, message.channel, message.channel.id
     if author == "PyBot":
         pass
@@ -449,6 +502,7 @@ async def on_message(message: discord.Message):
 
 @bot.listen()
 async def on_message_delete(message: discord.Message):
+    logging.info("Message delete processing")
     author, author_id, content, chanel, chanel_id = message.author.name, message.author.id, message.content, message.channel, message.channel.id
     db = func.db_history()
     cur = db.cursor()
@@ -485,6 +539,7 @@ async def on_message_delete(message: discord.Message):
 
 @bot.slash_command()
 async def get_my_avatar(ctx):
+    logging.info("the /get_my_avatar was used")
     embed = discord.Embed(title=f'Аватар пользователя {ctx.author.name}',color=discord.Color.green())
     embed.set_image(url=ctx.author.avatar)
     await ctx.respond(embed=embed)
@@ -492,6 +547,7 @@ async def get_my_avatar(ctx):
 
 @bot.slash_command()
 async def get_server_avatar(ctx):
+    logging.info("the /get_server_avatar was used")
     embed = discord.Embed(title=f'Аватар сервера {ctx.guild.name}',color=discord.Color.green())
     if ctx.guild.icon == None:
         await ctx.respond("У сервера нет иконки.")
@@ -506,6 +562,7 @@ async def get_server_avatar(ctx):
 
 @bot.slash_command(name='poll')
 async def create_poll(ctx, question: str,option1: str, option2:str, option3: Optional[str] = None, option4: Optional[str] = None, option5: Optional[str] = None, option6: Optional[str] = None, option7: Optional[str] = None, option8: Optional[str] = None, option9: Optional[str] = None, option10: Optional[str] = None):
+    logging.info("the /create_poll was used")
     if await func.moder(ctx) == True:
         await ctx.respond("Обработка...")
         reactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '10️⃣']
@@ -557,6 +614,7 @@ async def create_poll(ctx, question: str,option1: str, option2:str, option3: Opt
 
 @bot.slash_command()
 async def show_poll(ctx, message_id: str):
+    logging.info("the /show_poll was used")
     if await func.moder(ctx) == True:
         try:
             message = await ctx.channel.fetch_message(message_id)
@@ -592,6 +650,8 @@ async def show_poll(ctx, message_id: str):
 
 
 if __name__ == "__main__":
+    logging.info("hist_db init...")
     func.db_hist_init()
+    logging.info("bot starting...")
     bot.run(config.TOKEN)
 
---
+++ src/log.py
@@ -0,0 +1,3 @@
+import logging
+
+logging.basicConfig(filename='bot.log', level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
